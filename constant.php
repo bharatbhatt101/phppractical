27154dffc125ba5a95ef638dca5b1c44f489f2c0
bharatbhatt101 <bharatbhatt2003bk@gmail.com>
2023-10-06 19:35:48 +0530

    go

---
+++ constant.php
@@ -0,0 +1,32 @@
+<!-- Constants in php -->
+
+<html>
+<head>
+    
+    <title>Constant in php</title>
+</head>
+<body>
+    <h2>Constant using define() function</h2>
+        <?php
+         define("MESSAGE","Hello!This is PHP programming");
+         echo MESSAGE;
+         echo "<br>";
+        
+         
+
+         ?>
+
+<h2>Const keyword example</h2>
+        <?php
+         const message="Hello!This is PHP programming";
+         echo message;
+         
+        
+         
+
+         ?>
+         
+    
+
+</body>
+</html>
