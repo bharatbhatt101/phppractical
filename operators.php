107e3910ec10ec1cadaae4d4969472c4c82c6a19
bharatbhatt101 <bharatbhatt2003bk@gmail.com>
2023-10-09 23:31:11 +0530

    go

--- operators.php
+++ operators.php
@@ -10,34 +10,96 @@
                $a=20;
                $b=5;
 
-               echo "Addition of two numbers=".$a+$b;
+               echo "Sum: $a+$b=".$a+$b;
                echo "<br>";
-               echo "Subtraction of two numbers=".$a-$b;
+               echo "Sub: $a-$b =".$a-$b;
                echo "<br>";
-                echo "Multipliction of two numbers=".$a*$b;
+                echo "Multiply: $a*$b =".$a*$b;
                echo "<br>";
-                echo "Division of two numbers=".$a/$b;
+                echo "Divide: $a/$b =".$a/$b;
          ?>
 
         
         <h2>Logical Operators</h2>
         <?php
-          
+               $var=false;
+               $var1=true;
+
+               echo var_dump($var&&$var1);
+               echo "<br>";
+
+               echo var_dump($var||$var1);
+
 
          ?>
          
          <h2>Comparision Operators</h2>
          <?php
-       
+                  $c=40;
+                  $d=20;
+                  
+                  echo "$c==$b:";
+                  echo var_dump($c==$d)."<br>";
+                  echo "$c!=$b:";
+                                    
+                  echo var_dump($c!=$d)."<br>";
+                  echo "$c<$b:";
+                  
+                  echo var_dump($c<$d)."<br>";
+                  echo "$c>$b:";
+                  
+                  echo var_dump($c>$d)."<br>";
+
+
+
           ?>
 
           <h2>Assignment Operators</h2>
           <?php
-
+              
+              $x=10;
+              $y=20;
+              $z=15;
+              $m=25;
+                 
+              echo "x:$x <br>";
+              echo "x+=4:".$x+=4;
+              echo "<br>y:$y <br>";
+              echo "y-=1:".$y-=1;
+              echo "<br>z:$z <br>";
+              echo "z*=4:".$z*=5;
+              echo "<br>m:$m <br>";
+              echo "m/=5:".$m/=5;
         ?>
 
         <h2>Increment/Decrement Operators</h2>
          <?php
+                
+            //  $j=20;
+            //  $k=15;
+            //  $l=25;
+             
+             for ($i=0; $i <2 ;++$i)
+              { 
+                  
+                  echo "Pre-increment:".$i;
+
+
+             }
+             
+             for ($j=0; $j <2 ;$j++)
+             { 
+                 
+                 echo "Post-increment:".$j;
+
+
+            }
+            
+                
+            //   echo "Post-increment:".$j++;
+            //   echo "Pre-decrement:".--$k;
+            //   echo "Post-decrement:".$l--;
+
            
           ?>
           
